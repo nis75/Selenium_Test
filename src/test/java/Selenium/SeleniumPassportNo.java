982d2735dec19aa260891e183033734a9f173cdb
unknown <Administrator@Niamul-qc.fintechhub.com.bd>
2024-08-12 11:43:28 +0600

    first commit

---
+++ src/test/java/Selenium/SeleniumPassportNo.java
@@ -0,0 +1,14 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumPassportNo {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+}
